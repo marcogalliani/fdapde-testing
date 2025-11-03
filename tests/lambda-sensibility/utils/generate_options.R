60c89337fea175df3a435841b7e7064f030ea03a
Marco Galliani <105008364+marcogalliani@users.noreply.github.com>
2025-11-03 21:16:15 +0100

    Update options.

--- tests/lambda-sensibility/utils/generate_options.R
+++ tests/lambda-sensibility/utils/generate_options.R
@@ -20,17 +20,17 @@ generate_options <- function(test_suite, name_main_test, path_queue) {
   # - model_names: used for indexing (no spaces, please)
   # - model_labels: used for plotting
   model_names <- c(
-    "subspace", "sequential", "direct"
+    "subspace", "sequential"
   )
   model_labels <- c(
-    "subspace", "sequential", "direct"
+    "subspace", "sequential"
   )
   
   ## Define the color palette
   model_colors <- brewer.pal(length(model_labels), "Set1")
   
   ## Options that you want to be common across tests 
-  lambda_grid <- 10^seq(-6,1,by=1)
+  lambda_grid <- 10^seq(-4,0,by=1)
   seed <- 1412
   
   switch(
@@ -45,16 +45,16 @@ generate_options <- function(test_suite, name_main_test, path_queue) {
         cpp_script = "fPCA-2D",
         test_options = list(
           n_reps = 10,
-          varying_options = c("lambda")
+          varying_options = c("lambda","n_nodes","n_locs","n_stat_units")
         ),
         domain_and_locations = list(
           name_mesh = "unit_square",
           locs_eq_nodes = FALSE
         ),
         dimensions = list(
-          n_nodes = 400,       # vectors to be combined
-          n_locs = 400,   # vectors to be combined
-          n_stat_units = 50,
+          n_nodes = c(1000),       # vectors to be combined
+          n_locs = c(600),   # vectors to be combined
+          n_stat_units = c(100),
           n_nodes_HR_grid = 1000
         ),
         model_options = list(
@@ -62,11 +62,11 @@ generate_options <- function(test_suite, name_main_test, path_queue) {
         ),
         data = list(
           mean = FALSE,
-          var_pct = c(0.6,0.4) #pct of variance explained by each PC (w.r.t. to true data)
+          var_pct = c(0.7,0.3) #pct of variance explained by each PC (w.r.t. to true data)
 
         ),
         noise = list(
-          NSR = 0.10,    # vectors to be combined
+          NSR = 1.0,    # vectors to be combined
           seed = seed
         ),
         regularization = list(
@@ -80,6 +80,9 @@ generate_options <- function(test_suite, name_main_test, path_queue) {
           name_main_test,
           ## Include all the varying options!
           "lambda", sprintf("%.7f", comb_row$lambda),
+          "nn", sprintf("%.d", comb_row$n_nodes),
+          "nl", sprintf("%.d", comb_row$n_locs),
+          "nsu", sprintf("%.d", comb_row$n_stat_units),
           sep = "_"
         )
       }
